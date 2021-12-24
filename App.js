c7de660a94cf7e6d84a29510bb9e9f50704ed3e7
BerkZorbey <berktaha1994@gmail.com>
2021-12-24 03:27:23 +0300

    deneme

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+function App() {
+  return (
+    <View>
+      <Text>Merhaba</Text>
+    </View>
+  );
+}
+
+export default App;
